f24e554e29c7fe28ba2d5b2518eafecad4e229b1
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2026-08-12 13:18:27 +0900

    feat: export storyboard adapter

--- usecases/__init__.py
+++ usecases/__init__.py
@@ -1,3 +1,9 @@
+from .storyboard import KlingStoryboardAdapter, KlingStoryboardCompiler, KlingStoryboardPlan
 from .text_to_video import GenerateTextToVideoUseCase
 
-__all__ = ["GenerateTextToVideoUseCase"]
+__all__ = [
+    "GenerateTextToVideoUseCase",
+    "KlingStoryboardAdapter",
+    "KlingStoryboardCompiler",
+    "KlingStoryboardPlan",
+]
